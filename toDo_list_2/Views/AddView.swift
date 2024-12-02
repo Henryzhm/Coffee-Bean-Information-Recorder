e6e0fd78e280a48bce271d3c5f2e75c3db8c5b1c
huimingz <huiming.zhang@student.unimelb.edu.au>
2024-12-03 02:11:14 +1100

    first commit

---
+++ toDo_list_2/Views/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  toDo_list_2
+//
+//  Created by 张会明 on 2024/11/26.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
